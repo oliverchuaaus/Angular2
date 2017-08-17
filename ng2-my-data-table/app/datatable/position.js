b0606fd500badd2b66200082bc14d6142b8a521a
Oliver <oliver.chua.aus@gmail.com>
2017-08-17 14:23:47 +1000

    Delete ng2-table

--- ng2-my-data-table/app/datatable/position.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var PositionRecord = (function () {
-    function PositionRecord() {
-    }
-    return PositionRecord;
-}());
-exports.PositionRecord = PositionRecord;
-//# sourceMappingURL=position.js.map
